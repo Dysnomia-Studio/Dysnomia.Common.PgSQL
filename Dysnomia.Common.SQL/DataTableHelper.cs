3cec8f295d84b6b39634f862b8b4a505d66ba726
Elanis <elanis@hotmail.com>
2019-12-20 15:48:34 +0100

    Added helper to create DataTable from custom object

---
+++ Dysnomia.Common.SQL/DataTableHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace Dysnomia.Common.SQL {
+	public static class DataTableHelper {
+		public static DataTable CreateDataTableFromObject<T>(T myObject) {
+			Type type = typeof(T);
+			var properties = type.GetProperties();
+
+			DataTable dataTable = new DataTable();
+			List<object> values = new List<object>();
+			foreach(PropertyInfo info in properties) {
+				var value = info.GetValue(myObject);
+
+				if((!value.GetType().IsPrimitive && !(value is string) && !(value is DateTime)) ||
+					value is IEnumerable) {
+
+					continue;
+				}
+
+				if(value == null) {
+					value = DBNull.Value;
+				}
+
+				dataTable.Columns.Add(new DataColumn(info.Name, Nullable.GetUnderlyingType(info.PropertyType) ?? info.PropertyType));
+				values.Add(value);
+			}
+
+			dataTable.Rows.Add(values.ToArray());
+
+			return dataTable;
+		}
+
+	}
+}
